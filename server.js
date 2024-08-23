e8ba6fec8eefb0be8c14ebbe2c582ea489cfb6e3
JenniGabriely01 <lopesdossantonsjennifergabriely@gmail.com>
2024-08-23 16:30:11 -0300

    Adiciona teste para a conexão do back com o front

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const cors = require('cors');  // Certifique-se de importar o pacote cors
 const app = express();
 
 const PORT = 3000;
+
+// Configurando o CORS
+app.use(cors());
+
 /* Dados usuarios */
 const userSchema = new mongoose.Schema({
   name: String,
@@ -10,9 +15,7 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-
 const Usuarios = mongoose.model('User', userSchema);
-
 
 /* Conexão com o MongoDB (Porta padrão) */
 mongoose.connect('mongodb://localhost:27017/library-system', {
@@ -22,12 +25,13 @@ mongoose.connect('mongodb://localhost:27017/library-system', {
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Failed to connect to MongoDB', err));
 
-
-app.get('/', (req, res) => {
-  res.send('');
+app.get('/testeApi', (req, res) => {
+  res.json([{'titulo': 'Testando conexão front com back'}]);
 });
 
-
+/* app.get('/', (req, res) => {
+  Rota do login
+}); */
 
 app.listen(PORT, () => {
   console.log(`o Servidor iniciou na porta ${PORT} `);
