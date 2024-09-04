acc9f3acabd5e7805b33b739cfd91dbda687c9ec
JenniGabriely01 <lopesdossantonsjennifergabriely@gmail.com>
2024-09-04 16:28:28 -0300

    Criptografa a senha do banco de dados

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
 const cors = require('cors');
 
 const app = express();
@@ -14,7 +15,6 @@ app.use(bodyParser.json());
 
 /* Conexão com o banco de dados */
 mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
     useUnifiedTopology: true
 })
 .then(() => console.log('Conectado ao MongoDB'))
@@ -25,7 +25,37 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true }
 });
 
+// Middleware para criptografar a senha antes de salvar
+userSchema.pre('save', async function(next) {
+    const user = this;
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 10);
+    }
+    next();
+});
+
 const User = mongoose.model('User', userSchema);
+
+/* Criar usuário pré cadastrado */
+async function criarUsuario() {
+    const email = 'admin@example.com';
+    const plainPassword = 'adminpassword'
+
+    try {
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+            const newUser = new User({ email, password: plainPassword });
+            await newUser.save();
+            console.log('Usuário pré-cadastrado criado com sucesso');
+        } else {
+            console.log('Usuário já existe');
+        }
+    } catch (error) {
+        console.error('Erro ao criar usuário pré-cadastrado:', error);
+    }
+}
+
+criarUsuario();
 
 app.post('/login', async (req, res) => {
     const { email, password } = req.body;
@@ -35,13 +65,14 @@ app.post('/login', async (req, res) => {
     }
 
     try {
-        const user = await User.findOne({ email: email });
+        const user = await User.findOne({ email });
 
         if (!user) {
             return res.status(404).json({ error: 'Usuário não encontrado' });
         }
 
-        if (user.password === password) {
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (isPasswordValid) {
             res.status(200).json({ message: 'Login bem-sucedido' });
         } else {
             res.status(400).json({ error: 'Email ou senha incorretos' });
@@ -51,6 +82,7 @@ app.post('/login', async (req, res) => {
         res.status(500).json({ error: 'Erro ao autenticar usuário' });
     }
 });
+
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
