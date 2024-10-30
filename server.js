5360cf2d799453e6c8d4d9df9f426491c31e52be
JenniGabriely01 <lopesdossantonsjennifergabriely@gmail.com>
2024-10-30 16:54:13 -0300

    Adiciona rota para listar clientes e genêros principais

--- server.js
+++ server.js
@@ -109,6 +109,73 @@ async function sendMail(mailOptions) {
     }
 }
 
+/* --------- rotas para o dashboard ---------- */
+// Rota para obter os gêneros mais emprestados com numeração
+router.get('/api/generos-principais', async (req, res) => {
+    try {
+        const generosMaisEmprestados = await Emprestimo.aggregate([
+            { $unwind: "$livros" },
+            {
+                $lookup: {
+                    from: "livros",
+                    localField: "livros",
+                    foreignField: "_id",
+                    as: "livroInfo"
+                }
+            },
+            { $unwind: "$livroInfo" },
+            { $group: { _id: "$livroInfo.genero", totalEmprestimos: { $sum: 1 } } },
+            { $sort: { totalEmprestimos: -1 } },
+            { $limit: 4 } // Número de gêneros a ser exibido
+        ]);
+
+        // Adicionando numeração a cada item do array de gêneros
+        const generosNumerados = generosMaisEmprestados.map((genero, index) => ({
+            posicao: index + 1,
+            genero: genero._id,
+            totalEmprestimos: genero.totalEmprestimos
+        }));
+
+        res.status(200).json(generosNumerados);
+    } catch (error) {
+        console.error('Erro ao buscar gêneros mais emprestados:', error);
+        res.status(500).json({ message: 'Erro ao buscar gêneros mais emprestados', error });
+    }
+});
+
+router.get('/api/clientes-principais', async (req, res) => {
+    try {
+        const clientesQueMaisEmprestam = await Emprestimo.aggregate([
+            { $unwind: "$cliente" },
+            {
+                $lookup: {
+                    from: 'clientes',
+                    localField: 'cliente',
+                    foreignField: '_id',
+                    as: 'clienteInfo'
+                }
+            },
+            { $unwind: "$clienteInfo" },
+            { $group: { _id: "$clienteInfo.nome", totalClientes: { $sum: 1 } } },
+            { $sort: { totalClientes: -1 } },
+            { $limit: 4 }
+        ]);
+
+
+        const clienteNumerados = clientesQueMaisEmprestam.map((cliente, index) => ({
+            posicao: index + 1,
+            cliente: cliente._id,
+            totalClientes: cliente.totalClientes
+        }));
+
+        res.status(200).json(clienteNumerados);
+    } catch (error) {
+        console.error('Erro ao buscar clientes:', error);
+        res.status(500).json({ message: 'Erro ao buscar clientes', error });
+    }
+});
+
+
 /* Rota para cadastrar cliente */
 router.post('/api/clientes', async (req, res) => {
     const { nome, sobrenome, email, telefone } = req.body;
